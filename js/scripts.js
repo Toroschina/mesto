5bf01d6ab54535f8461a22625f0aefe010c527c1
Toroschina <e.toroschina@asmart-app.ru>
2022-07-08 10:34:56 +0300

    08-07-2022
    Исправление для мобильной версии

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-  const buttonEdit = document.querySelector('.button__edit');
-  const buttonClose = document.querySelector('.button__close');
+  const buttonEdit = document.querySelector('.button_edit');
+  const buttonClose = document.querySelector('.button_close');
   const popup = document.querySelector('.popup');
 
   function popupOpen(evt) {
