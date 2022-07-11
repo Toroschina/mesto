54a6b02eb7e0778e2a13e3cbb4569ff1050337cb
Toroschina <e.toroschina@asmart-app.ru>
2022-07-11 11:37:28 +0300

    11-07-2022
    Исправление от ревьювера

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,28 @@
   const buttonClose = document.querySelector('.popup__button-close');
   const popup = document.querySelector('.popup');
 
-  function popupOpen() {
-    popup.classList.add('popup_opened');
-  }
+  let formElement = document.querySelector('.popup__container');
+  let nameInput = formElement.querySelector('.popup__text-input_name');
+  let jobInput = formElement.querySelector('.popup__text-input_description');
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
 
+  function popupOpen() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+    popup.classList.add('popup_opened');
+
+  }
   function popupClose() {
     popup.classList.remove('popup_opened');
   }
 
+  function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    popupClose();
+  }
   buttonEdit.addEventListener('click', popupOpen);
   buttonClose.addEventListener('click', popupClose);
-
-  let formElement = document.querySelector('.popup');
-  let nameInput = formElement.querySelector('user-name');
-  let jobInput = formElement.querySelector('user-title');
-
-  function formSubmitHandler() {
-    nameInputText = nameInput.value;
-    jobInputText = jobInput.value;
-    profileName.textContent = nameInputText;
-    profileDescription.textContent = jobInputText;
-    popup.classList.remove(popupClose);
-}
-
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__button-edit');
   formElement.addEventListener('submit', formSubmitHandler);
