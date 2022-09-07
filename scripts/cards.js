86eb5bc76f2edecb70f9b62dcfb65353da350f97
Toroschina <e.toroschina@asmart-app.ru>
2022-09-07 12:45:21 +0300

    07-09-2022
    ПР6

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: "Severus Snape",
+    link: "http://images4.fanpop.com/image/photos/24400000/Harry-Potter-Wallpaper-harry-potter-24478423-1024-768.jpg",
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: "Draco Malfoy",
+    link: "https://a.d-cd.net/ea12421s-1920.jpg",
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: "Ron Harry Hermione",
+    link: "https://funik.ru/wp-content/uploads/2018/11/1bcd42419df04bf32854.jpg",
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: "Harry Potter",
+    link: "https://dl-media.viber.com/5/share/2/long/vibes/roll/image/400/24c7/a159f4ec1622f3a50337dd6b22c8b14a9854f46c8291118538b8ef68572a24c7.jpg",
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: "Ron Weasley",
+    link: "https://i.pinimg.com/originals/04/17/3e/04173e092ee094bb4b236289c6787f8f.jpg",
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    name: "Hermione Granger",
+    link: "https://v1.popcornnews.ru/k2/news/1200/upload/i9pzlc.jpg",
   },
 ]; // массив карточек
